41f30c513b7c8ba0b7163b52caae023c6da72535
Jmart2424 <admin@heydaydigitalmedia.com>
2025-09-23 06:14:59 -0700

    Update GmailV2.node.ts- CHANGED BACK

--- packages/nodes-base/nodes/Google/Gmail/v2/GmailV2.node.ts
+++ packages/nodes-base/nodes/Google/Gmail/v2/GmailV2.node.ts
@@ -306,7 +306,7 @@ export class GmailV2 implements INodeType {
 
 						let appendAttribution = options.appendAttribution;
 						if (appendAttribution === undefined) {
-							appendAttribution = false;
+							appendAttribution = nodeVersion >= 2.1;
 						}
 
 						const email: IEmail = {
